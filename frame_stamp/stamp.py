a1a2686b2069d206d918a12126ad86b529580d69
p.giydenko <p.giydenko@cgfww.ru>
2020-03-05 12:17:47 +0300

    update todo add ci scripts

--- frame_stamp/stamp.py
+++ frame_stamp/stamp.py
@@ -131,7 +131,7 @@ class FrameStamp(object):
             self.set_source(input_image)
         if not self.source:
             raise RuntimeError('Source image not set')
-        logger.debug(f'Start stamping with template "{self.template["name"]}"')
+        # logger.debug(f'Start stamping with template "{self.template["name"]}"')
         # формат файла
         img_size = self.source.size
         for shape in self.get_shapes():     # type: BaseShape
