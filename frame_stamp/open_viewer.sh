99dc5844ce6e7e2c34efce269004cd4b769e8a57
p.giydenko <p.giydenko@cgfww.ru>
2020-05-05 14:08:39 +0300

    move launcher

--- frame_stamp/open_viewer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# activate virtual env before start
-export PYTHONPATH="$(realpath $( dirname $( dirname "${BASH_SOURCE[0]}" )))"
-source /mnt/toolbox/pipeline/cgf_libs/all/cgf_tools/env.sh
-echo $PYTHONPATH
-cd "$( dirname "${BASH_SOURCE[0]}" )"/viewer
-python ./dialog.py
