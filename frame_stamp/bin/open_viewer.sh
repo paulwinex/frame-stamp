54f51af50a12d5ac0df2d508392d33f51f6f1e3c
paulwinex <paulwinex@gmail.com>
2023-09-11 14:50:36 +0300

    updated ui launcher

--- frame_stamp/bin/open_viewer.sh
+++ frame_stamp/bin/open_viewer.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# activate virtual env before start
-PYPATH=$(readlink -f "$(dirname $( readlink -f "${BASH_SOURCE:-$0}"))"/../..)
-export PYTHONPATH=$PYPATH
-cd ../viewer
-python3 dialog.py
+
+PYPATH=$(readlink -f "$(dirname $( readlink -f "${BASH_SOURCE:-$0}"))"/..)
+export PYTHONPATH="$(dirname $PYPATH)"
+python3 $PYPATH/viewer/dialog.py
