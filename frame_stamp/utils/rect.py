275be0d785aa02e76161cde94b5de9bd6ca5c691
paulwinex <paulwinex@gmail.com>
2024-11-16 16:19:43 +0300

    update rect util

--- frame_stamp/utils/rect.py
+++ frame_stamp/utils/rect.py
@@ -53,7 +53,7 @@ class Rect:
         return not (self.right < other.left or self.left > other.right or
                     self.bottom < other.top or self.top > other.bottom)
 
-    def contains(self, point: Point):
+    def contains(self, point: Point|list|tuple):
         """
         Проверяет, содержит ли прямоугольник указанную точку.
 
@@ -63,6 +63,9 @@ class Rect:
         Возвращает:
         True, если прямоугольник содержит точку, иначе False
         """
+        if isinstance(point, (list, tuple)):
+            assert len(point) == 2, 'Point must be a list or tuple of length 2'
+            point = Point(*point)
         return (self.left <= point.x <= self.right and
                 self.top <= point.y <= self.bottom)
 
@@ -82,6 +85,33 @@ class Rect:
         mapped_x = other_rect.left + local_x
         mapped_y = other_rect.top + local_y
         return Point(mapped_x, mapped_y)
+
+    def corners(self, as_tuple=True):
+        values = self.top_right.tuple, self.bottom_left.tuple
+        if as_tuple:
+            return tuple(x.tuple for x in values)
+        return values
+
+    def point(self, as_tuple=True):
+        values = self.top_left, self.top_right, self.bottom_right, self.bottom_left
+        if as_tuple:
+            return tuple(x.tuple for x in values)
+        return values
+
+    def line(self, as_tuple=True):
+        values = self.top_left, self.top_right, self.bottom_right, self.bottom_left, self.top_left
+        if as_tuple:
+            return tuple(x.tuple for x in values)
+        return values
+
+    def adjusted(self, left, top, right, bottom):
+        return Rect(
+            self._x + left,
+            self._y + top,
+            self._width - left - right,
+            self._height - top - bottom
+        )
+
 
     def __str__(self):
         return f"Rect({self._x}, {self._y}, {self._width}, {self._height})"
